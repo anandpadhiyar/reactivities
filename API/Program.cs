ea2611b21f64c273a9d93090b8a261d4e034c023
Anand <anand@acedataanalytics.com>
2022-01-21 21:44:57 +0530

    End of section 2

---
+++ API/Program.cs
@@ -0,0 +1,47 @@
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var ConnectionString = builder.Configuration.GetConnectionString("Default");
+builder.Services.AddDbContext<DataContext>(options => {
+    options.UseSqlServer(ConnectionString);
+});
+
+
+
+var app = builder.Build();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try {
+    var context = services.GetRequiredService<DataContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedData(context);
+} catch(Exception err) {
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(err, "An error occurred during migration");
+}
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+await app.RunAsync();
